0c56f0f7a6b6966132dbf30bfcb3b41f58af6493
EvgeniyMoz <157408501+EvgeniyMoz@users.noreply.github.com>
2024-07-05 20:35:55 +0300

    Add files via upload

---
+++ Leson4/TeacherView.java
@@ -0,0 +1,12 @@
+/*group6380.Mozyakov; */
+
+import java.util.List;
+
+public class TeacherView implements UserView<Teacher>{
+    @Override
+    public void sendOnConsole(List<Teacher> list) {
+        for (Teacher teacher : list) {
+            System.out.println(teacher);
+        }
+    }
+}
